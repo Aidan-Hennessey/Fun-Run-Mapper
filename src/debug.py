f37965c3221668d1bb354d0f8843f2a4c6194248
Jason Eveleth <jj11@eveleth.com>
2023-05-14 15:00:46 -0400

    Made debug.py modular

--- src/debug.py
+++ src/debug.py
@@ -1,24 +1,27 @@
 import http.client
 import urllib.parse
 
-content = """hello
-80948 90328 4098 2903
-39048 20398 40392
-483209 493028 403928 49320 
-9038 409832 09423 4302
-"""
-
-headers = {
-    'content-type': 'application/x-www-form-urlencoded',
-}
-
 HOST = 'sky.jason.cash'
 PORT = 8080
 
-conn = http.client.HTTPSConnection(HOST, PORT)
+def request(content):
+    conn = http.client.HTTPSConnection(HOST, PORT)
 
-body = 'full_data=' + urllib.parse.quote(content)
-conn.request('POST', '/', body=body, headers=headers)
-r = conn.getresponse()
-print(r.read().decode())
+    headers = { 'content-type': 'application/x-www-form-urlencoded', }
+    body = 'full_data=' + urllib.parse.quote(content)
+    conn.request('POST', '/', body=body, headers=headers)
 
+    r = conn.getresponse()
+    return r.read().decode()
+
+def main():
+    content = """hello
+    80948 90328 4098 2903
+    39048 20398 40392
+    483209 493028 403928 49320 
+    9038 409832 09423 4302
+    """
+    print(request(content))
+
+if __name__ == "__main__":
+    main()
